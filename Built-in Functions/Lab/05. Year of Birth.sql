1edcebc6c53f32577153672c86f33797ef915e95
Emil Roydev <e.roydev@gmail.com>
2024-04-19 12:30:06 -0700

    updates

--- Built-in Functions/Lab/05. Year of Birth.sql
+++ Built-in Functions/Lab/05. Year of Birth.sql
@@ -11,3 +11,11 @@ SELECT
 	last_name,
 	EXTRACT('year' FROM born) AS year
 FROM authors
+
+------------------------------------------
+
+SELECT
+	first_name,
+	last_name,
+	TO_CHAR(born, 'YYYY') AS year
+FROM authors
