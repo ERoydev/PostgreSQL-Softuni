9e3509dfc57fa5d6286335114871952c6d5a471c
Emil Roydev <e.roydev@gmail.com>
2024-04-30 04:40:13 -0700

    exercises

--- Built-in Functions/Exercise/05. Substring River Length.sql
+++ Built-in Functions/Exercise/05. Substring River Length.sql
@@ -1,3 +1,10 @@
 SELECT
 	SUBSTRING("River Information" FROM '([0-9]{1,4})') AS river_length
 FROM view_river_info
+
+
+-----------------------------
+
+SELECT
+	(REGEXP_MATCHES("River Information", '([0-9]{1,4})'))[1] AS river_info
+FROM view_river_info
